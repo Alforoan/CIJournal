5743406a8eef0f4b31f4fdaf95e09920018a3fc9
Jay <jdragon2050@gmail.com>
2024-04-11 19:46:44 -0700

    add logic for reading and writing persistent data

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
+import fs from 'fs';
+import path from 'path';
 
 const app = express();
+
 const port = 3000;
 
-
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
+);
 app.use(bodyParser.json());
 
 
@@ -13,11 +22,73 @@ app.get('/', (req, res) => {
 });
 
 
-app.post('/api/data', (req, res) => {
-  const requestData = req.body;
+async function appendToFile(filePath, data) {
+  try {
+    await fs.promises.appendFile(filePath, data);
+    console.log(`Data appended to file: ${data}`);
+  } catch (err) {
+    console.error('Error appending to file:', err);
+    throw err; 
+  }
+}
 
-  res.json({ message: 'Data received successfully', data: requestData });
+async function readAndParseFile(filePath) {
+  try {
+    const data = await fs.promises.readFile(filePath, 'utf8');
+    const parsedData = data
+      .split('\n')
+      .filter((entry) => entry.trim() !== '')
+      .map(JSON.parse);
+    return parsedData;
+  } catch (err) {
+    console.error('Error reading file:', err);
+    throw err; 
+  }
+}
+
+app.get('/api/journals', async (req, res) => {
+  try {
+
+    const filePath = 'journals.txt';
+    if (!fs.existsSync(filePath)) {
+      console.log(`File does not exist: ${filePath}`);
+      return res.status(404).json({ error: 'File not found' });
+    }
+
+   
+    const journals = await readAndParseFile(filePath);
+
+ 
+    res.json({ journals });
+  } catch (err) {
+    console.error('Error handling GET request:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
+
+app.post('/api/journals', async (req, res) => {
+  const { title, date, journal } = req.body;
+
+  const jsonData = JSON.stringify({ title, date, journal }) + '\n';
+
+  try {
+    const filePath = 'journals.txt';
+    if (!fs.existsSync(filePath)) {
+      await fs.promises.writeFile(filePath, '');
+      console.log(`File created: ${filePath}`);
+    }
+
+
+    await appendToFile(filePath, jsonData);
+
+
+    res.json({ message: 'Data received and appended to file successfully' });
+  } catch (err) {
+    console.error('Error handling POST request:', err);
+    res.status(500).json({ error: 'Internal Server Error' }); 
+  }
+});
+
 
 
 app.listen(port, () => {
